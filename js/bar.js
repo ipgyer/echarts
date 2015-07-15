0c2c69597c3cc10e65b2d9b86c17797f3a0e44c0
wangyang <wyang@yy.com>
2015-07-15 17:34:44 +0800

    新发布demo

---
+++ js/bar.js
@@ -0,0 +1,68 @@
+require.config({
+    paths: {
+        echarts: 'js'
+    }
+});
+
+require(
+	[
+        'echarts',
+        'echarts/chart/bar' // 使用柱状图就加载bar模块，按需加载
+    ],
+    function(ec){
+    	var myChart = ec.init(document.getElementById('wrap')); 
+
+    	var option = {
+    		title:{
+    			text:'柱状图',
+    			subtext:'纯属虚构',
+    			x:'center'
+    		},
+    		tooltip : {
+		        trigger: 'axis'
+		    },
+		    grid:{
+		    	backgroundColor:'#f1f1f3',
+		    	borderWidth:0,
+		    	borderColor:'#f1f1f3',
+		    	z:1
+		    },
+		    areaStyle:{
+		    	color:'#FF0',
+		    	type:'default'
+		    },
+    		series:[
+    			{
+    				type:'bar',
+    				data:[1.0,2.1,3.2,4.3,5.4,6.5,7.6],
+    				barCategoryGap: '60%',
+    				itemStyle:{
+		    			normal:{
+		    				color:'#00bffe',
+		    				barBorderRadius:[5, 5, 0, 0],
+		    				label:{
+							    show: true,
+							    position:'top',
+							    textStyle:{
+							    	color:'#00bffe'
+							    }
+							} 
+		    			}
+		    		}
+    			}
+    		],
+    		xAxis:[
+    			{
+    				data:['先秦诸子百家','近代文学','国外文学','历史试卷类','散文类','论述类文本阅读','实用类文本阅读']
+    			}
+    		],
+    		yAxis:[
+    			{
+    				type:'value'
+    			}
+    		]
+    	}
+    	// 为echarts对象加载数据 
+        myChart.setOption(option); 
+    }
+)
---
+++ js/pie.js
@@ -0,0 +1,118 @@
+// 路径配置
+        require.config({
+            paths: {
+                echarts: 'js'
+            }
+        });
+        
+        // 使用
+        require(
+            [
+                'echarts',
+                'echarts/chart/pie' // 使用柱状图就加载bar模块，按需加载
+            ],
+            function (ec) {
+                // 基于准备好的dom，初始化echarts图表
+                var myChart = ec.init(document.getElementById('main')); 
+                
+                var option = {
+                    title:{
+                    	text:"站点初步数据统计",
+                    	subtext:"纯属虚构",
+                    	x:'center'
+                    },
+                    toolbox:{
+                    	show:true,
+                    	x:'right',
+                    	/*feature:{
+                    		mark : {
+						        show : true,
+						        title : {
+						            mark : '辅助线开关',
+						            markUndo : '删除辅助线',
+						            markClear : '清空辅助线'
+						        },
+						        lineStyle : {
+						            width : 2,
+						            color : '#1e90ff',
+						            type : 'dashed'
+						        }
+						    },
+						    dataZoom : {
+						        show : true,
+						        title : {
+						            dataZoom : '区域缩放',
+						            dataZoomReset : '区域缩放后退'
+						        }
+						    },
+						    dataView : {
+						        show : true,
+						        title : '数据视图',
+						        readOnly: true,
+						        lang: ['数据视图', '关闭', '刷新']
+						    },
+						    magicType: {
+						        show : true,
+						        title : {
+						            line : '折线图切换',
+						            bar : '柱形图切换',
+						            stack : '堆积',
+						            tiled : '平铺',
+						            force: '力导向布局图切换',
+						            chord: '和弦图切换',
+						            pie: '饼图切换',
+						            funnel: '漏斗图切换'
+						        },
+						        option: {
+						            // line: {...},
+						            // bar: {...},
+						            // stack: {...},
+						            // tiled: {...},
+						            // force: {...},
+						            // chord: {...},
+						            // pie: {...},
+						            // funnel: {...}
+						        },
+						        type : []
+						    },
+						    restore : {
+						        show : true,
+						        title : '还原'
+						    },
+						    saveAsImage : {
+						        show : true,
+						        title : '保存为图片',
+						        type : 'png',
+						        lang : ['点击保存']
+						    }
+                    	}*/
+                    },
+                    legend:{
+                    	orient:'vertical',
+                    	data:['容易','简单','一般','较难','困难'],
+                    	x:'left'
+                    },
+                    series:[
+	                    	{
+	                    	type:'pie',
+	                    	name:'哎呦，我去！',
+	                    	clickable:false,
+	                    	center:['50%','60%'],
+	                    	radius:'50%',
+	                    	calculable:true,
+	                    	data:[
+	                    		{value:100,name:'容易',itemStyle:{normal:{color:'#fbb320'}}},
+	                    		{value:200,name:'简单',itemStyle:{normal:{color:'#00bffe'}}},
+	                    		{value:300,name:'一般',itemStyle:{normal:{color:'#b400ff'}}},
+	                    		{value:400,name:'较难',itemStyle:{normal:{color:'#fa6175'}}},
+	                    		{value:500,name:'困难',itemStyle:{normal:{color:'#00d1a2'}}}
+	                    	]
+	                    }
+                    ]
+
+                };
+        
+                // 为echarts对象加载数据 
+                myChart.setOption(option); 
+            }
+        );
